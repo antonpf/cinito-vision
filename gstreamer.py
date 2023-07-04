e5eea28a1d3375a766384b5bc19cddda56336659
Anton Pfeifer <anton.pfeifer@th-owl.de>
2023-07-04 16:40:27 +0200

    test save image

--- gstreamer.py
+++ gstreamer.py
@@ -139,19 +139,19 @@ class GstPipeline:
             # Passing Gst.Buffer as input tensor avoids 2 copies of it.
             gstbuffer = gstsample.get_buffer()
 
-            # Save image
-            result, mapinfo = gstbuffer.map(Gst.MapFlags.READ)
-            if result:
-                print("Saving image ...")
-                caps = gstsample.get_caps()
-                width = caps.get_structure(0).get_value('width')
-                height = caps.get_structure(0).get_value('height')
-                print("Width: ", width)
-                print("Height: ", height)
-                img = Image.frombytes('RGB', (width, height), mapinfo.data, 'raw')
-                img.save("/mnt/test.jpg")
-                img.close()
-                gstbuffer.unmap(mapinfo)
+            # # Save image
+            # result, mapinfo = gstbuffer.map(Gst.MapFlags.READ)
+            # if result:
+            #     print("Saving image ...")
+            #     caps = gstsample.get_caps()
+            #     width = caps.get_structure(0).get_value('width')
+            #     height = caps.get_structure(0).get_value('height')
+            #     print("Width: ", width)
+            #     print("Height: ", height)
+            #     img = Image.frombytes('RGB', (width, height), mapinfo.data, 'raw')
+            #     img.save("/mnt/test.jpg")
+            #     img.close()
+            #     gstbuffer.unmap(mapinfo)
 
             svg = self.user_function(gstbuffer, self.src_size, self.get_box())
             if svg:
